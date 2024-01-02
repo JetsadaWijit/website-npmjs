25d4568a368de8424ba6a0104dd6d7386c729842
owen <56456776+JetsadaWijit@users.noreply.github.com>
2024-01-02 03:01:34 +0000

    Change logic

--- docs/script/dashboard/version.js
+++ docs/script/dashboard/version.js
@@ -3,9 +3,12 @@ function redirectToVersion() {
     
     // Validate the entered version (you may want to add more validation)
     if (/^\d+\.\d+\.\d+$/.test(enteredVersion)) {
-        var versionPath = "version/" + enteredVersion.replace(/\./g, '_') + "/";
+        var versionComponents = enteredVersion.split(".");
+        
+        var versionPath = "version/" + versionComponents[0] + "/" + versionComponents[1] + "/" + versionComponents[2] + "/";
         var versionFileName = "index.html";
         var fullPath = versionPath + versionFileName;
+        
         window.location.href = fullPath;
     } else {
         alert("Please enter a valid version number (e.g., 1.0.0).");
